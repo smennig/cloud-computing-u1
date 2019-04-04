e56b95b852149e351c2093eb4dad6254cfdddb87
Simon Mennig <simon.mennig@hotmail.de>
2019-04-04 16:43:38 +0200

    Added file upload

--- src/main/java/htwg/konstantz/cloud/ex1/Ex1Application.java
+++ src/main/java/htwg/konstantz/cloud/ex1/Ex1Application.java
@@ -1,9 +1,14 @@
 package htwg.konstantz.cloud.ex1;
 
+import htwg.konstantz.cloud.ex1.configuration.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class Ex1Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/htwg/konstantz/cloud/ex1/controller/ImageController.java
@@ -0,0 +1,92 @@
+package htwg.konstantz.cloud.ex1.controller;
+
+import htwg.konstantz.cloud.ex1.entities.Image;
+import htwg.konstantz.cloud.ex1.entities.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+
+@RestController()
+public class ImageController {
+
+    private static final String DOWNLOAD_FILE_PATH = "downloadFileById";
+    @Autowired
+    private ImageRepository repo;
+
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    public void saveImage() {
+
+    }
+
+    @GetMapping("/images")
+    List<Image> all() {
+        return repo.findAll();
+    }
+
+    @PostMapping("/images")
+    public Image saveImage(@RequestBody Image image) {
+
+        return repo.save(image);
+    }
+
+    @PostMapping("uploadImage/{id}")
+    public String uploadImage(@RequestParam("file") MultipartFile file, @PathVariable long id) {
+        String fileName = fileStorageService.storeFile(file);
+
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/" + DOWNLOAD_FILE_PATH + "/")
+                .path("" + id)
+                .toUriString();
+
+        Image image = repo.findById(id).get();
+        image.setPath(fileDownloadUri);
+        image.setFileName(fileName);
+        ;
+        repo.save(image);
+        return fileDownloadUri;
+    }
+
+    @GetMapping("/images/{id}")
+    public Image findImageById(@PathVariable Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+
+    @GetMapping("/" + DOWNLOAD_FILE_PATH + "/{id}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable Long id, HttpServletRequest request) {
+
+        String fileName = repo.findById(id).get().getFileName();
+
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+}
